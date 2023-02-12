1d799e19e6a1a00e07e729412c12f0169823c416
Akhilendra <47774234+Akhilendra01@users.noreply.github.com>
2023-02-12 16:57:42 +0530

    Create AdaptiveSegmentTree.cpp

---
+++ AdaptiveSegmentTree.cpp
@@ -0,0 +1,78 @@
+/*
+	author: akhilendra11
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+#define endl "\n"
+
+struct SegmentTree{
+private:
+	int *st, n, DEAD;
+	int (*comb)(int, int);
+public:
+	SegmentTree(int n, int (*comb)(int, int), int DEAD=0){
+		this->n=n;
+		this->comb=comb;
+		this->DEAD=DEAD;
+		st=(int*)malloc(sizeof(int)*(this->n)*4);
+		for(int i=0;i<4*n;i++)this->st[i]=DEAD;	
+	}
+	void build(int *a, int lx, int rx, int x){
+		if(lx==rx){st[x]=a[lx]; return;}
+		int mid=(lx+rx)>>1;
+		build(a, lx, mid, 2*x+1);
+		build(a, mid+1, rx, 2*x+2);
+		st[x]=comb(st[2*x+1], st[2*x+2]);
+	}
+	void build(int *a){
+		build(a, 0, n-1, 0);
+	}
+	int query(int l, int r, int lx, int rx, int x){
+		if(rx<l or r<lx)return DEAD;
+		if(l<=lx and rx<=r)return st[x];
+		int mid=(lx+rx)>>1;
+		int q1=query(l, r, lx, mid, 2*x+1); 
+		int q2=query(l, r, mid+1, rx, 2*x+2); 
+		return comb(q1, q2);
+	}
+	int query(int l, int r){
+		return query(l, r, 0, n-1, 0);
+	}
+	void update(int idx, int val, int lx, int rx, int x){
+		if(lx==rx){st[x]=val;	return;}
+		int mid=(lx+rx)>>1;
+		if(idx<=mid)update(idx, val, lx, mid, 2*x+1);
+		else update(idx, val, mid+1, rx, 2*x+2);
+		st[x]=comb(2*x+1, 2*x+2);
+	}
+	void update(int idx, int val){
+		update(idx, val, 0, n-1, 0);
+	}
+};
+
+
+
+int comb(int a, int b){return max(a, b);}
+
+int32_t main(){
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+	int n; cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)cin>>a[i];
+
+	SegmentTree tree(n, comb, 0);	tree.build(a);
+	cout<<tree.query(1, 5)<<"\n";
+	tree.update(2, 0);	cout<<tree.query(1, 5);
+
+	return 0;
+}
